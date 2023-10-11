12d742fb9cdc22a590328e34b2fd59e02b2ded71
Anne Giralt <giralt.anne@gmail.com>
2023-10-11 09:01:26 +0200

    feat : added visualisation for regression and classification
    Added plot_regression and plot_classification in the tools.py file and use it in the notebook figure.ipynb on biolog_iML1515_EXP_UB.

--- tools.py
+++ tools.py
@@ -3,6 +3,10 @@ from sklearn.base import BaseEstimator, TransformerMixin
 import keras
 import tensorflow as tf
 from loss import SV_loss, V_in_loss, V_pos_loss
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_auc_score, roc_curve
+import seaborn as sns
+
 
 
 class MaxScaler(BaseEstimator,TransformerMixin):
@@ -111,3 +115,36 @@ def custom_loss(S, P_out, P_in):
         return keras.losses.mean_squared_error(y_true,L)
     return my_mse 
 
+def plot_regression(pred, true, pred_label, true_label, title, saving_file=None):
+    plt.title(title)
+    sns.set(font='arial', palette="colorblind", style="whitegrid", font_scale=2.5, rc={'figure.figsize':(11,11)})
+    sns.regplot(x=true, y=pred, fit_reg=0, marker='+', color='black', scatter_kws={'s':40, 'linewidths':0.7})
+    plt.xlabel(true_label)
+    plt.ylabel(pred_label)
+    plt.xlim(min(true)-0.01, max(true)+0.05)
+    plt.ylim(min(pred)-0.02, max(pred)+0.05)
+
+    if saving_file:
+        plt.savefig(saving_file, format="png", dpi=600, bbox_inches='tight')
+    plt.show()
+    plt.close("all")
+
+def plot_classification(pred, true, saving_file=None):
+
+    roc = roc_curve(true, pred)
+    auc = roc_auc_score(true, pred)
+    plt.title('AUC = %.2f'% auc)
+
+    sns.set(font='arial', palette="colorblind", style="whitegrid", font_scale=2.5, rc={'figure.figsize':(11,11)})
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.ylim((0,1))
+    plt.xlim((0,1))
+    plt.xticks([0.2,0.4,0.6,0.8,1.0])
+    sns.lineplot(x=roc[0], y=roc[1])
+
+    if saving_file:
+        plt.savefig(saving_file, format="png", dpi=600, bbox_inches='tight')
+
+    plt.show()
+    plt.close("all")
