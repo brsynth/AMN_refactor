55d90865b49052df72d3585a2616328f3a7cc9f8
Anne Giralt <giralt.anne@gmail.com>
2023-11-30 09:45:52 +0100

    feat : zip model
    Add function to zip and unzip the keras model files

--- amn/amn/tools.py
+++ amn/amn/tools.py
@@ -98,3 +98,44 @@ def threshold_percentage_max(X,percentage):
     max = X.max()
     X_threshold = X >= max*percentage
     return X_threshold.astype(int)
+
+
+import os
+import pathlib 
+import zipfile
+from pathlib import Path
+
+
+def unzip_folders(main_directory):
+    """Extract all the files from all the subdirectory of 
+    the given directory."""
+    sub_dir = os.listdir(main_directory)
+    for d in sub_dir:
+        if zipfile.is_zipfile(main_directory + d):
+            unzip_folder(main_directory, d)
+
+def unzip_folder(path_directory, folder_name):
+    """Extract all files from the given folder of the given
+    directory into this directory."""
+    file = path_directory + folder_name
+    print("Extracting",file)
+    with zipfile.ZipFile(file, 'r') as zip_ref:
+        zip_ref.extractall(path_directory)
+
+
+def zip_models(model_directory):
+    """Transform every file with ".keras" suffix in the given model
+    directory into a compressed archive."""
+    for file_name in os.listdir(model_directory):
+        if pathlib.Path(model_directory + file_name).suffix == ".keras":
+            zip_file(model_directory, file_name)
+
+def zip_file(path_directory, file_name):
+    """Transform a given file into a compressed archive in the same given
+    directory."""
+    file = path_directory + file_name
+    archive_path = path_directory + Path(file_name).stem + ".zip"
+    print("Create archive",archive_path)
+    zf = zipfile.ZipFile(archive_path, "w", zipfile.ZIP_DEFLATED)
+    zf.write(file, file_name)
+    os.remove(file)
